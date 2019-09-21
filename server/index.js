ce529938c82af4d23f3832b537d5d75f49946f06
ubervelocity <mihaitu.popescu@gmail.com>
2019-09-21 18:32:05 +0200

    fixed deployment path

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ const sim = require('./routes/api/sim');
 app.use('/api/sim', sim);
 
 // Handle production
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Static folder
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public/'));
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
